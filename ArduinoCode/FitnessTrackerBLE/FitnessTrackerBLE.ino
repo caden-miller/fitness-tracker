d00aab71a3238c9cd8292a46e916e253c4108be3
caden-miller <cadenmiller40@gmail.com>
2024-04-16 21:36:26 -0500

    moved to expo app for android, updated README.md. trying to get heart rate BPM detection going.

--- ArduinoCode/FitnessTrackerBLE/FitnessTrackerBLE.ino
+++ ArduinoCode/FitnessTrackerBLE/FitnessTrackerBLE.ino
@@ -1,47 +1,123 @@
 #include <Wire.h>
-#include <SparkFun_MAX3010x_Sensor_Playground.h>
 #include <ArduinoBLE.h>
+#include "MAX30105.h"
+#include "heartRate.h"
 
-MAX3010x sensor;
+MAX30105 particleSensor;
+void updateValues();
 
-BLEService fitnessTrackerService("180D"); // 180D is the UUID of the Heart Rate service
-BLEIntCharacteristic heartRateMeasurementChar("2A37", BLERead | BLENotify); // 2A37 is the UUID of the Heart Rate Measurement characteristic
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+
+float beatsPerMinute;
+float beatAvg;
+
+const char * UUID_fitnessTrackerService = "84582cd0-3df0-4e73-9496-29010d7445dd";
+const char * UUID_heartRateMeasurementCharacteristic = "84582cd1-3df0-4e73-9496-29010d7445dd"; 
+
+BLEService fitnessTrackerService(UUID_fitnessTrackerService); // 180D is the UUID of the Heart Rate service
+BLEFloatCharacteristic chHRAVG(UUID_heartRateMeasurementCharacteristic, BLERead | BLENotify); // 2A37 is the UUID of the Heart Rate Measurement characteristic
 
 void setup() {
+  
   Serial.begin(9600);
-  while (!Serial);
+  Serial.println("Initializing...");
 
+  // Initialize sensor
+  Serial.println("Trying to start sensor.");
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) {
+    while (1) {
+      Serial.println("MAX30102 was not found. Please check wiring/power. ");
+    }
+  }
+
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+
+  Serial.println("Trying to start BLE.");
   if (!BLE.begin()) {
-    Serial.println("starting BLE failed!");
-    while (1);
+    while (1) {
+      Serial.println("starting BLE failed!");
+    }
   }
 
-  BLE.setLocalName("FitnessTracker");
+  BLE.setLocalName("PeripheralArduino");
   BLE.setAdvertisedService(fitnessTrackerService);
-  fitnessTrackerService.addCharacteristic(heartRateMeasurementChar);
+  fitnessTrackerService.addCharacteristic(chHRAVG);
   BLE.addService(fitnessTrackerService);
-  heartRateMeasurementChar.writeValue(0);
+  chHRAVG.writeValue(0);
   BLE.advertise();
-
-  if (!sensor.begin(Wire, I2C_SPEED_FAST)) {
-    Serial.println("MAX3010x did not start. Check wiring/power. ");
-    while (1);
-  }
 }
 
 void loop() {
+  static long preMillis = 0;
+
+  
+
   // Check if a central device has connected
   BLEDevice central = BLE.central();
   if (central) {
     Serial.print("Connected to central: ");
     Serial.println(central.address());
-    while (central.connected()) {
-      long irValue = sensor.getIR(); // Read IR data from the sensor
-      if (irValue > 0) {
-        heartRateMeasurementChar.writeValue(irValue); // Send IR data over BLE
-      }
-    }
+    while (central.connected()) 
+      {
+      // additional placeholder for writing command from central
+      // to this device. "myCharacteristic" is a characteristic initialized
+      // in write mode (BLEwrite) with his own UUid
+      /*
+      if (myCharacteristic.written()) 
+        {
+        command = myCharacteristic.value(); // retrieve value sent from central
+        Serial.print(F("commmand value:  "));
+        Serial.println(command);
+        }
+      */
+      
+      long curMillis = millis();
+      if (preMillis>curMillis) preMillis=0; // millis() rollover?
+      if (curMillis - preMillis >= 10) // check values every 10mS
+        {
+        preMillis = curMillis;
+        updateValues(); // call function for updating value to send to central
+        }
+     } // still here while central connected
+
+    // central disconnected:
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
+  } // no central
     Serial.print("Disconnected from central: ");
     Serial.println(central.address());
+    Serial.print("Mac Address is "); Serial.println(WiFi.macAddress());
+}
+
+
+void updateValues()
+{
+  long irValue = particleSensor.getIR();
+  if (checkForBeat(irValue) == true) {
+    //We sensed a beat!
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20) {
+      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+      rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
   }
+
+  chHRAVG.writeValue(beatsPerMinute);
 }
--- README.md
+++ README.md
@@ -4,32 +4,30 @@ Fitness tracker that also detects heart rate and pulse ox anomalies.
 Implemented using an Arduino Nano 33 BLE and MAX30102 heart rate and pulse ox sensor.
 
 ## Installation
- - install Cordova using Node.js 
- - ```npm install -g cordova```
+ - install project using Node.js
+ - ```cd fitness-tracker-app``` 
+ - ```npm install```
 
-## Running
- - connect computer to android phone
- - make sure phone is connected to bluetooth
- - prepare app
- - ```cordova prepare android```
- - build app
- - ```cordova build android```
- - run app
- - ```cordova run android```
-
-## Alternate Running
- - download APK directly via github
- - make sure phone is connected to bluetooth and also is able to install apps from unknown sources
- - located in ```CordovaApp/platforms/android/app/build/outputs/apk/debug/app-debug.apk```
- -
+## Running 
+ - setup eas 
+ - ```npx npm install eas-cli```
+ - ```npx expo install expo-dev-client```
+ - run using eas
+ - ```eas build --profile development --platform android```
+ - scan QR code once built
+ - install on Android
+ - scan for devices
+ - accept permissions
+ - get heart rate
 
 ## Plans
- - setup android app
- - get BLE working
- - add heart sensor support
- - send heart sensor data to app
- - add [Edge Impulse](https://edgeimpulse.com/) support for exercise detection
- - send exercise data to app
+ - verify app can connect to Arduino
+ - verify BLE is transmitting HR data
+ 
+ - finalize 3D model
+ - verify heart rate data
+ - 
+ - make battery work 
  - finalize app UI
  - test
  - finalize and polish
